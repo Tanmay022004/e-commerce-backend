bbb314c7d717fbd7fc890bda7d03c1556392dba7
Tanmay Gedam <tanmaygedam111@gmail.com>
2026-05-01 20:54:32 +0530

    initial commit

---
+++ controllers/paymentController.js
@@ -0,0 +1,9 @@
+exports.simulatePayment = (req, res) => {
+  const success = Math.random() > 0.2;
+
+  if (!success) {
+    return res.status(400).json({ msg: "Payment failed" });
+  }
+
+  res.json({ msg: "Payment successful" });
+};
