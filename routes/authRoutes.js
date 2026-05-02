b645cc0cb5da19c435e3b375c158c501ecef755e
Tanmay Gedam <tanmaygedam111@gmail.com>
2026-05-02 20:04:32 +0530

    test register route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const { register, login } = require("../controllers/authController");
 
-router.post("/register", register);
+router.post("/register", (req, res) => {
+  res.json({ msg: "register route works" });
+});
 router.post("/login", login);
 
 module.exports = router;
